80dbb2116123228c3e51cc2f1923f0f6841baadd
Kari <kari@Karinas-MacBook-Pro.local>
2022-04-09 18:15:30 +0300

    Create Models and Word Class for Dictionary App

---
+++ Models/Word.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DictionaryWebApp.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string WordText { get; set; }
+        public string WordMeaning { get; set; }
+        public string WordTranslate { get; set; }
+        public string WordUsageExample { get; set; }
+
+        public Word()
+        {
+
+        }
+    }
+}
