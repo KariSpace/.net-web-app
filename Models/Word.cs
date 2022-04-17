bbcdf6e7fb2e11306247dea71ba550f0d6a7a866
Karina Pivtorak <pivtorak.k@handy.ai>
2022-04-17 17:04:36 +0300

    Validation finalization

--- Models/Word.cs
+++ Models/Word.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace DictionaryWebApp.Models
 {
     public class Word
     {
 
         public int Id { get; set; }
-
+        [Required]
         public string WordText { get; set; }
-
+        [Required]
         public string WordMeaning { get; set; }
-
+        [Required]
         public string WordTranslate { get; set; }
-
+        [Required]
         public string WordUsageExample { get; set; }
 
         public Word()
